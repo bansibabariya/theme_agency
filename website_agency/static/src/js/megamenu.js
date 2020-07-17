c040ea9f17f17db282b945feced5f8d9fd4891fb
Sophia L <sophia.opensys@gmail.com>
2020-07-17 22:03:06 +0530

    [ADD]code added

--- website_agency/static/src/js/megamenu.js
+++ website_agency/static/src/js/megamenu.js
@@ -16,26 +16,26 @@ $( document ).ready(function() {
   $('.js-animation-link').each(function () {
     var showAnimation = new HSShowAnimation($(this)).init();
   });
-
-  // initialization of slick carousel
-  $('.js-slick-carousel').each(function() {
-    var slickCarousel = $.HSCore.components.HSSlickCarousel.init($(this));
-  });
-
-  // initialization of form validation
-  $('.js-validate').each(function() {
-    $.HSCore.components.HSValidation.init($(this), {
-      rules: {
-        confirmPassword: {
-          equalTo: '#signupPassword'
-        }
-      }
-    });
-  });
-
-  // initialization of go to
-  $('.js-go-to').each(function () {
-    var goTo = new HSGoTo($(this)).init();
-  });
+//
+//  // initialization of slick carousel
+//  $('.js-slick-carousel').each(function() {
+//    var slickCarousel = $.HSCore.components.HSSlickCarousel.init($(this));
+//  });
+//
+//  // initialization of form validation
+//  $('.js-validate').each(function() {
+//    $.HSCore.components.HSValidation.init($(this), {
+//      rules: {
+//        confirmPassword: {
+//          equalTo: '#signupPassword'
+//        }
+//      }
+//    });
+//  });
+//
+//  // initialization of go to
+//  $('.js-go-to').each(function () {
+//    var goTo = new HSGoTo($(this)).init();
+//  });
 
 });
